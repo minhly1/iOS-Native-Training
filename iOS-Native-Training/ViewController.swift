acd274ad2f2fcca4b11525039e0a900ea62637e4
Ly Kien Minh <min.ly@terralogic.com>
2025-12-01 14:09:31 +0700

    Initial Commit

---
+++ iOS-Native-Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Native-Training
+//
+//  Created by MIN on 1/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-Native-TrainingTests/iOS_Native_TrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Native_TrainingTests.swift
+//  iOS-Native-TrainingTests
+//
+//  Created by MIN on 1/12/25.
+//
+
+import Testing
+@testable import iOS_Native_Training
+
+struct iOS_Native_TrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
