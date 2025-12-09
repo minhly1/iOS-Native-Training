56a988088aa7b0ea0d40bf748c6c8a6677e0b92f
Ly Kien Minh <min.ly@terralogic.com>
2025-12-09 18:39:43 +0700

    Add UICollectionViewCell

---
+++ Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  iOS-Native-Training
+//
+//  Created by MIN on 9/12/25.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
